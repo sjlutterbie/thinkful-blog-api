db693c85a9c18a44ffa400825ee1b809375cd41c
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-09-27 16:26:32 +0000

    Implemented runServer and closeServer functionality

--- server.js
+++ server.js
@@ -17,7 +17,39 @@ app.use(express.static('public'));
 const blogPostRouter = require('./blogPostRouter');
   app.use('/blog-posts',blogPostRouter);
 
-// Launch app
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// Set up runServer & closeServer for testing
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () =>{
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on('error', err =>{
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+// If this file is called directly, launch the server
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
