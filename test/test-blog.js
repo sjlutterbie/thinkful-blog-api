fe4cfd2619848d915fd4e380df300cc45ef2e306
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-09-27 16:47:10 +0000

    Updated .travis.yml; started test files

--- test/test-blog.js
+++ test/test-blog.js
@@ -1,3 +1,30 @@
 // Load testing tools
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+
+// Load items for running server
+const {app, runServer, closeServer} = require('../server');
+
+// Simplify use of chai expect
+const expect = chai.expect;
+
+// Load HTTP testing into Chai
+chai.use(chaiHttp);
+
+// Test '/blog-posts' operations
+describe('Blog posts', function() {
+  
+  // Start server before running tests
+  before(function() {
+    return runServer();
+  });
+  
+  // Close server after running tests
+  after(function() {
+    return closeServer();
+  });
+  
+  
+  
+  
+})
