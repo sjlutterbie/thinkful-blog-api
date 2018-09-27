2bfffaabff188ac72b0bd28476d20b60b1d82a42
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-09-27 16:54:36 +0000

    Created GET method test

--- test/test-blog.js
+++ test/test-blog.js
@@ -24,7 +24,37 @@ describe('Blog posts', function() {
     return closeServer();
   });
   
+  // Test GET method
+  it('should list blog posts on GET', function() {
+    
+    // Run test
+    return chai
+      .request(app)
+      .get('/blog-posts')
+      .then(function(res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        
+        // Confirm there are posts
+        expect(res.body.length).to.be.at.least(1);
+        
+        //Confirm each object has the correct keys
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(post){
+          expect(post).to.be.a('object');
+          expect(post).to.include.keys(expectedKeys);
+        });
+      });
+  });
   
+  // Test GET /:id method
+  
+  // Test POST method
+  
+  // Test PUT method
+  
+  // Test DELETE method
   
   
 })
